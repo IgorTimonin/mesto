26b221e69b9ff9478faee5d8c3d64aaf61086ea7
Igor Timonin <mut4nt@yandex.ru>
2022-03-01 12:51:10 +0300

    js update

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,11 @@ const popup = document.querySelector('.popup');
 const openPopup = document.querySelector('.profile__edit-btn');
 const closePopup = popup.querySelector('.popup__btn-close');
 let saveBtn = document.querySelector('.popup__btn-save');
-let profileName = document.querySelector('.profile__name')
-let profileOccupation = document.querySelector('.profile__occupation')
-let inputName = document.querySelector('.popup__profile-name')
-let inputOccupation = document.querySelector('.popup__profile-occupation')
-let likeBtn = document.querySelectorAll('.gallery__like-btn')
+let profileName = document.querySelector('.profile__name');
+let profileOccupation = document.querySelector('.profile__occupation');
+let inputName = document.querySelector('.popup__profile-name');
+let inputOccupation = document.querySelector('.popup__profile-occupation');
+let likeBtn = document.querySelectorAll('.gallery__like-btn');
 let name = 'ВВедите имя';
 
 console.log(likeBtn);
@@ -17,7 +17,7 @@ function togglePopup() {
   inputOccupation.value = profileOccupation.textContent;
 };
 
-popup.addEventListener('click', function(event) {
+popup.addEventListener('click', function (event) {
   if (event.target === event.currentTarget) {
     togglePopup();
   }
@@ -30,7 +30,7 @@ function inputProfileInfo() {
   profileName.textContent = inputName.value;
   profileOccupation.textContent = inputOccupation.value;
   if (inputName.value.length < 1) {
-    profileName.textContent = 'Укажите имя';
+    profileName.textContent = 'Неизвестный';
   }
   if (inputOccupation.value.length < 1) {
     profileOccupation.textContent = 'Чем вы занимаетесь?';
