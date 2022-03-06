f01dd99b1cf485688cfccee973d000c383d686c4
IgorTimonin <mut4nt@yandex.ru>
2022-03-06 12:34:58 +0300

    review 1 fix js

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,28 @@
 const popup = document.querySelector('.popup');
 const openPopup = document.querySelector('.profile__edit-btn');
 const closePopup = popup.querySelector('.popup__btn-close');
-let saveBtn = document.querySelector('.popup__btn-save');
 let profileName = document.querySelector('.profile__name');
-let profileOccupation = document.querySelector('.profile__occupation');
-let inputName = document.querySelector('.popup__profile-name');
-let inputOccupation = document.querySelector('.popup__profile-occupation');
-let likeBtn = document.querySelectorAll('.gallery__like-btn');
-let name = 'ВВедите имя';
+let profileJob = document.querySelector('.profile__job');
+let formElement = document.querySelector('.popup__profile-form');
+let inputName = formElement.querySelector('.popup__field_type_name');
+let inputJob = formElement.querySelector('.popup__field_type_job');
 
 function togglePopup() {
   popup.classList.toggle('popup_opened');
-  inputName.value = profileName.textContent;
-  inputOccupation.value = profileOccupation.textContent;
-};
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  if (inputName.value.length < 1) {
+    profileName.textContent = 'Неизвестный';
+  }
+  if (inputJob.value.length < 1) {
+    profileJob.textContent = 'Чем вы занимаетесь?';
+  }
+  togglePopup();
+}
 
 popup.addEventListener('click', function (event) {
   if (event.target === event.currentTarget) {
@@ -22,23 +31,8 @@ popup.addEventListener('click', function (event) {
 });
 
 openPopup.addEventListener('click', togglePopup);
+inputName.value = profileName.textContent;
+inputJob.value = profileJob.textContent;
+
 closePopup.addEventListener('click', togglePopup);
-
-function inputProfileInfo() {
-  profileName.textContent = inputName.value;
-  profileOccupation.textContent = inputOccupation.value;
-  if (inputName.value.length < 1) {
-    profileName.textContent = 'Неизвестный';
-  }
-  if (inputOccupation.value.length < 1) {
-    profileOccupation.textContent = 'Чем вы занимаетесь?';
-  }
-  togglePopup();
-};
-saveBtn.addEventListener('click', inputProfileInfo);
-
-for (let i = 0; i < likeBtn.length; i++) {
-  likeBtn[i].onclick = function () {
-    this.classList.toggle('gallery__like-btn_active');
-  }
-}
+formElement.addEventListener('submit', formSubmitHandler);
